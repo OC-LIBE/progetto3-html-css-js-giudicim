6b9b31a4d42f393e5cdc1a33be01ad830b2253a4
giudicim <magagilu77@gmail.com>
2025-06-11 09:39:58 +0200

    ultima lezione

--- main.js
+++ main.js
@@ -6,29 +6,87 @@ async function getPetsData() {
     return petsdata
 }
 
-async function displayPets() {
+
+async function displayPets(){
+
     const pets = await getPetsData();
-    const template = document.querySelector
-    (`#animal-card-template`)
+    const template = document.querySelector('#animal-card-template')
 
-    const wrapper = document.querySelector(`main`)
+    const wrapper = document.querySelector('main')
 
-    console.log(template)
-
-    pets.forEach(pet => {
-        const clone= template.content.cloneNode(true)
-        //qui modifichiamo il template
-
-        //aggiorniamo la foto
-        const image = clone.querySelector('.animal-card-photo img')
-        image.src = pet.photo
-
-        //console.log(pet.photo)  
-
-        //aggiungiamo l'articolo alal pagina
-        wrapper.appendChild(clone)
+      function ageText(age) {
+        if (age > 1) {
+            return age + ' years old'
+        } else if (age == 1) {
+            return age + ' year old'
+        } else {
+            return 'less than a year old'
+        }
     }
-); 
+
+
+    pets.forEach( pet =>{
+        const clone = template.content.cloneNode(true)
+
+        // qui modifichiamo il template
+        // Nome animale
+        const name = clone.querySelector('.animal-name')
+        name.textContent=pet.name;
+
+        const ageclass = clone.querySelector('.animal-age')
+        const d = new Date();
+        let age = d.getFullYear() - pet.birthYear;
+        ageclass.innerText = ageText(age)
+
+        const species = clone.querySelector('.species');
+        species.textContent=pet.species.charAt(0).toUpperCase() + pet.species.slice(1);
+
+        const description = clone.querySelector('.animal-description')
+        description.textContent = pet.description;
+        clone.querySelector('.btn-name').textContent = pet.name
+
+        const adoptBtn = clone.querySelector('.adopt-button')
+        // adoptBtn.action = `${website}/pets/${pet.id}/`
+        adoptBtn.href = `${website}/pets/${pet.id}/`
+        // aggiorniamo la foto
+        const image = clone.querySelector('.animal-card-photo img')
+        image.src=pet.photo
+        // console.log(pet.photo)
+
+        //aggiungiamo l'articolo alla pagaina
+        wrapper.appendChild(clone)
+
+        }
+    )
 
 }
+
+
 displayPets()
+function displayFiltersAnimal(e) {
+    let petsArticles = document.querySelectorAll("article")
+
+    for (let index=0; index <petsArticles.length; index++) {
+        let petsArticles = petsArticles[index]
+        const small= petsArticles.querySelectorAll("animal-card-text small span")
+
+        if (e.target.filterAnimal == "All") {
+            petsArticles,style.display = "flex"
+        }
+        else if (e.target.filterAnimal == "All") {
+            petsArticles,style.display = "flex"
+        }
+        else if (e.target.filterAnimal == "All") {
+            petsArticles,style.display = "flex"
+        }
+        
+    console.log(e.target.dataset.filter)
+}
+};
+const filterButtons = document.querySelectorAll("nav button");
+
+filterButtons.forEach(button=> {
+    button.addEventListener('click', (e) => {
+        displayFiltersAnimal(e);
+    })
+});
